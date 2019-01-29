5a3b41a6fc00cb6b78fb055276e9f5c5ee0f0c95
annie <1242947680@qq.com>
2019-01-29 18:47:56 +0800

    No 34

---
+++ src/Swordfinger/NO34.java
@@ -0,0 +1,15 @@
+package Swordfinger;
+
+public class NO34 {
+/**
+ * 题目：求第1500个丑数
+ * 丑数的定义，我们把因子只包含2/3/5的数据称为丑数
+ * @param args
+ */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
