19a15af54b0f78c9b24a8e731c7274505e8d0207
annie <1242947680@qq.com>
2019-01-30 11:31:32 +0800

    alter NO34

--- src/Swordfinger/NO34.java
+++ src/Swordfinger/NO34.java
@@ -6,10 +6,40 @@ public class NO34 {
  * 丑数的定义，我们把因子只包含2/3/5的数据称为丑数
  * @param args
  */
-	
+	public boolean isUgly(int number){
+		while (number%2==0) {
+			 number/=2;
+		}
+		while (number%3==0) {
+			number/=3;
+		}
+		while (number%5==0) {
+			number/=5;
+		}
+		if (number==1) {
+			return true;
+		}else {
+			return false;			
+		}
+	}
+	public int getUglyNumber(int index){
+		if (index<=0) {
+			return 0;
+		}
+		int number=0;
+		int uglyNumber = 0;
+		while (uglyNumber<index) {
+			number++;
+			if (isUgly(number)) {
+				uglyNumber++;
+			}
+			//前面number=1；此处number++
+		}
+		return number;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		NO34 no34 = new NO34();
+		System.out.println(no34.getUglyNumber(3));
 	}
-
 }
